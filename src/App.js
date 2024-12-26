a5314a7438afd5df0e82097abe261c7c1e9aeb1f
yedukondalu77 <yedukondalunadakuditi21@gmail.com>
2024-12-26 11:07:47 +0530

    first version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+// import './App.css';
+import BasicCrud from './Components/Basic_Crud';
+
+function App() {
+  return (
+    <div className="App">
+     <BasicCrud/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Basic_Crud.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Basic_Crud = () => {
+  return (
+    <div>
+      <h1>hiii</h1>
+    </div>
+  )
+}
+
+export default Basic_Crud
